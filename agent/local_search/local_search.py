498f8c7725923b2d0e5ad7dc8bcd13b622e02eb3
waltsai2483 <waltsai930830@gmail.com>
2025-05-20 00:15:41 +0800

    fix path start station logic

--- agent/local_search/local_search.py
+++ agent/local_search/local_search.py
@@ -6,12 +6,13 @@ import sys
 import time
 from typing import List, Tuple
 
+import pygame
+
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../src")
 
 from api.static import StaticStationGame
 
-
-game = StaticStationGame(gamespeed=50, visuals=False)
+game = StaticStationGame(gamespeed=25, visuals=False)
 
 def neighbors(state: Tuple[List[int], bool]) -> List[Tuple[List[int], bool]]:
     neighbors = []
@@ -25,12 +26,6 @@ def neighbors(state: Tuple[List[int], bool]) -> List[Tuple[List[int], bool]]:
             neighbor.append(station)
             neighbors.append((neighbor, False))
     
-    '''
-    for station in state[0]: # I don't think removing a station is a good idea
-        neighbor: list = copy.copy(state[0])
-        neighbor.remove(station)
-        neighbors.append((neighbor, False))
-    '''
     if len(state[0]) >= 3:
         neighbors = [(x[0], y) for x in neighbors for y in [False, True]]
     return neighbors
@@ -52,25 +47,37 @@ def local_search(previous_paths: List[Tuple[List[int], bool]] = [], start: Tuple
         
         for neighbor in neighbors_list:
             score = game.run(*previous_paths, neighbor)
-            if score >= best_score:
-                if score > best_score:
-                    best_paths.clear()
-                    best_score = score
-                best_paths.append(neighbor)
+            if score > best_score:
                 local_minima_found = False
+
+                best_paths.clear()
+                best_paths.append(neighbor)
+                best_score = score
         print(f'Iter thru {len(neighbors_list)} w/ score = {best_score}, t = {(datetime.datetime.now() - t).seconds} sec.; ')
         t = datetime.datetime.now()
 
     print(f'Found local minima! Path = {chosen_path}, Score = {best_score}')
 
-    game.visuals_on()
-    game.screenshot(f'./agent/local_search/results/local_search_{"_".join(map(str, chosen_path[0]))}{"_looped" if chosen_path[1] else ""}.png', chosen_path)
+    game.open_window()
+    game.screenshot(f'./agent/local_search/results/local_search_{len(previous_paths)}th_{"_".join(map(str, chosen_path[0]))}{"_looped" if chosen_path[1] else ""}.png', *previous_paths, chosen_path)
     print(f'Best Score: {game.run(*previous_paths, chosen_path)}')
-
+    game.close_window()
 
     return chosen_path
 
 best_paths = [local_search([], ([0], False))]
+covered_stations = list(range(len(game.mediator.stations)))
 for i in range(1, 7):
-    new_path = local_search(previous_paths=best_paths, start=([i], False))
+    if len(covered_stations) == 0:
+        start_station = random.randint(0, len(game.mediator.stations) - 1)
+    else:
+        start_station = covered_stations[0]
+    new_path = local_search(previous_paths=best_paths, start=([start_station], False))
+    for station in new_path[0]:
+        covered_stations.remove(station)
     best_paths.append(new_path)
+
+game.open_window()
+game.mediator.gamespeed = 10
+game.mediator.is_paused = True
+game.run(*best_paths)
